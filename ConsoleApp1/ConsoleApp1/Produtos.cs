23ee087449f1d969027cb11001a7624631de811a
luizalr <48811321+luizalr@users.noreply.github.com>
2019-04-25 20:04:46 -0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Produtos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Produtos
+    {
+          List<Produto> listaProdutos;
+  //      listaProdutos = new List<Produto>; merdaaaaa
+
+        public List<Produto> ListaProdutos {
+                 get { return listaProdutos; }
+                 set { listaProdutos = value; }
+            }
+
+        public Produtos()
+        {
+            this.ListaProdutos = new List<Produto>();
+      //      this.listaProdutos = new List<Produto>();
+        }
+
+
+        public void adicionar(Produto p)
+        {
+            this.ListaProdutos.Add(p);
+        }
+
+        public bool remover(Produto p)
+        {
+           Produto resultado =  this.pesquisar(p);
+           if(resultado.Codigo < 0)
+            {
+                return false;
+            }
+            else
+            {
+                this.ListaProdutos.Remove(resultado);
+                return true;
+            }
+
+        }
+
+        public Produto pesquisar(Produto p)
+        {
+            foreach(Produto elemento in this.ListaProdutos)
+            {
+                if(elemento.Equals(p))
+                {
+                    return elemento;
+                }
+            }
+
+            return new Produto();
+            
+        }
+
+        public double custoTotal()
+        {
+            double custo = 0;
+
+            foreach(Produto elemento in this.ListaProdutos)
+            {
+                custo += elemento.custo();
+            }
+            return custo;
+        }
+    }
+}
